c653ece07e02626145b6b5d1f2d6274d7555ea4b
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-07-07 10:28:26 -0300

    Caminho basilar de conexão com a Brapi

--- CarteiraInvestimentosAPI/Program.cs
+++ CarteiraInvestimentosAPI/Program.cs
@@ -1,6 +1,7 @@
 using CarteiraInvestimentos.Adapters.Driving.GlobalExceptionHandler;
 using CarteiraInvestimentos.Adapters.Driving.Validators;
 using CarteiraInvestimentos.Adapters.Infrastructure.Database;
+using CarteiraInvestimentos.Adapters.Infrastructure.ExternalServices;
 using CarteiraInvestimentos.Domain.Services;
 using CarteiraInvestimentos.Ports;
 using FluentValidation;
@@ -19,13 +20,23 @@ builder.Services.AddProblemDetails();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Injeção de dependências
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
-// Modifique esta linha no seu Program.cs
+
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         // Permite que a API entenda e responda Enums como Texto (Strings) no JSON
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
+// Registra o BrapiService configurando a URL base e o Header de Autorização de forma global
+builder.Services.AddHttpClient<IMercadoFinanceiroService, BrapiService>(client =>
+{
+    client.BaseAddress = new Uri("https://brapi.dev/");
+    client.Timeout = TimeSpan.FromSeconds(10);
+    
+    // Injeta o Token no formato 'Authorization: Bearer SEU_TOKEN' requerido pela produção da Brapi
+    client.DefaultRequestHeaders.Authorization = 
+        new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "API_KEY");
+});
 // Registra o Serviço de Negócio como SCOPED (Uma instância por requisição HTTP)
 builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 // Varre o projeto buscando todas as classes que herdam de AbstractValidator e as registra automaticamente
@@ -33,7 +44,6 @@ builder.Services.AddValidatorsFromAssemblyContaining<CreateTransactionDtoValidat
 
 var app = builder.Build();
 
-// ATENÇÃO: O escudo contra erros deve ser o primeiro componente do pipeline HTTP
 app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
